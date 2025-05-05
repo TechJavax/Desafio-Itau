a47b5580f2f749d2e6f64eef0e6bba41a7ebc115
França <mars.cesc@gmail.com>
2025-05-05 06:15:34 -0300

    Initial commit - Desafio API Itaú

---
+++ api_itau/src/main/java/desafio/api_itau/ApiItauApplication.java
@@ -0,0 +1,13 @@
+package desafio.api_itau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiItauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiItauApplication.class, args);
+	}
+
+}
